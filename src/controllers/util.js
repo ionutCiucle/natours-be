0615f1e610a091ed5ee0000d487fe780035b9c88
Ionut Ciucle <ionut.ciucle@gmail.com>
2023-10-04 07:11:37 +0300

    Add express query operator conversion to mongo-style operator

--- src/controllers/util.js
+++ src/controllers/util.js
@@ -1,12 +1,22 @@
 const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
-// const convertOperatorsToMongoSyntax = (query) =>
-//   query.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);x
+const convertOperatorsToMongoSyntax = (query) => {
+  const textQuery = JSON.stringify(query);
+  const mongoTextQuery = textQuery.replace(
+    /\b(gte|gt|lte|lt)\b/g,
+    (match) => `$${match}`,
+  );
+  const mongoQuery = JSON.parse(mongoTextQuery);
+
+  return mongoQuery;
+};
 
 const getFilteredQuery = (expressQuery) => {
-  const filteredQuery = Object.keys(expressQuery)
+  const mongoQuery = convertOperatorsToMongoSyntax(expressQuery);
+
+  const filteredQuery = Object.keys(mongoQuery)
     .filter((field) => excludedFields.indexOf(field) === -1)
-    .reduce((acc, curr) => ({ ...acc, [curr]: expressQuery[curr] }), {});
+    .reduce((acc, curr) => ({ ...acc, [curr]: mongoQuery[curr] }), {});
 
   return filteredQuery;
 };
